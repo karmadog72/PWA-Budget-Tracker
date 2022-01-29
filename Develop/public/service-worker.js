e9d441fa7630ffc9241af1da24caa2b7aae5f2a5
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2022-01-29 14:08:50 -0700

    added install, activate & fetch  requests

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -6,6 +6,7 @@ const FILES_TO_CACHE = [
   "./js/idb.js",
   "./js/index.js",
   "./css/styles.css",
+  "./manifest.json",
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
   "./icons/icon-128x128.png",
@@ -16,28 +17,6 @@ const FILES_TO_CACHE = [
   "./icons/icon-512x512.png",
 ];
 
-// Respond with cached resources
-self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        // if cache is available, respond with cache
-        console.log("responding with cache : " + e.request.url);
-        return request;
-      } else {
-        // if there are no cache, try fetching request
-        console.log("file is not cached, fetching : " + e.request.url);
-        return fetch(e.request);
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  );
-});
-
-// Cache resources
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
@@ -51,8 +30,6 @@ self.addEventListener("install", function (e) {
 self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
       let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       });
@@ -67,6 +44,21 @@ self.addEventListener("activate", function (e) {
           }
         })
       );
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
     })
   );
 });
